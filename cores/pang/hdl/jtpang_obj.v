7436e7bcac99732844952fe2899e817457164a87
Jose Tejada <jose.tejada@jotego.es>
2022-05-26 13:02:47 +0200

    Colours ok

--- cores/pang/hdl/jtpang_obj.v
+++ cores/pang/hdl/jtpang_obj.v
@@ -95,7 +95,7 @@ reg  [31:0] pxl_data;
 reg         match, dr_start, dr_busy, buf_we,
             wait_ok;
 
-assign buf_data  = { cur_pal,  pxl_data[30], pxl_data[31], pxl_data[28], pxl_data[29] }; // pxl_data[31:28] };
+assign buf_data  = { cur_pal, pxl_data[31:28] };
 assign scan_addr = { obj_cnt, sub_cnt };
 
 always @* begin
@@ -170,14 +170,14 @@ always @(posedge clk, posedge rst) begin
         if( dr_busy ) begin
             if( wait_ok && rom_ok && dr_pxl[2:0]==0 ) begin
                 pxl_data <= {
-                    rom_data[15], rom_data[11], rom_data[ 7], rom_data[ 3],
-                    rom_data[14], rom_data[10], rom_data[ 6], rom_data[ 2],
-                    rom_data[13], rom_data[ 9], rom_data[ 5], rom_data[ 1],
-                    rom_data[12], rom_data[ 8], rom_data[ 4], rom_data[ 0],
-                    rom_data[31], rom_data[27], rom_data[23], rom_data[19],
-                    rom_data[30], rom_data[26], rom_data[22], rom_data[18],
-                    rom_data[29], rom_data[25], rom_data[21], rom_data[17],
-                    rom_data[28], rom_data[24], rom_data[20], rom_data[16]
+                    rom_data[11], rom_data[15], rom_data[ 3], rom_data[ 7],
+                    rom_data[10], rom_data[14], rom_data[ 2], rom_data[ 6],
+                    rom_data[ 9], rom_data[13], rom_data[ 1], rom_data[ 5],
+                    rom_data[ 8], rom_data[12], rom_data[ 0], rom_data[ 4],
+                    rom_data[27], rom_data[31], rom_data[19], rom_data[23],
+                    rom_data[26], rom_data[30], rom_data[18], rom_data[22],
+                    rom_data[25], rom_data[29], rom_data[17], rom_data[21],
+                    rom_data[24], rom_data[28], rom_data[16], rom_data[20]
                 };
                 rom_addr[1] <= 1;
                 buf_we      <= 1;
